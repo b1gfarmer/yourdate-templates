3f0710e2755ff0dd8f4fa9dd2ccd49cc83156560
Danila Pachkov <inversopro@yandex.ru>
2025-07-21 15:50:22 +0300

    locally stable version

--- seating_plan/src/index.js
+++ seating_plan/src/index.js
@@ -1,2 +1,3 @@
 import './style.css';
 import './scripts/main.js';
+__webpack_public_path__ = '/yourdate-templates/seating_plan/';
--- seating_plan/webpack.config.js
+++ seating_plan/webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
-    publicPath: '/yourdate-templates/seating_plan/',
+    publicPath: '/',
   },
   mode: 'production',
   devServer: {
